1323d3e9fd63a686335173922fc3044f59224ba1
Comingsoon <comingsoon000@ya.ru>
2021-04-13 01:42:32 +0300

    refactor: use import for removeKFromList function

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,3 +1,4 @@
+const removeKFromList = require('./08-remove-from-list');
 const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -10,28 +11,6 @@ const ListNode = require('../extensions/list-node');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  *
  */
-
-const listFromArr = (arr) => {
-  const [first, ...rest] = arr;
-  const node = new ListNode(first);
-  node.next = (rest.length !== 0) ? listFromArr(rest) : null;
-  return node;
-};
-
-function removeKFromList(l, k) {
-  const arrFromListWithoutInt = (list, int, acc = []) => {
-    if (list.value !== int) {
-      acc.push(list.value);
-    }
-    if (list.next !== null) {
-      arrFromListWithoutInt(list.next, int, acc);
-    }
-    return acc;
-  };
-  const arr = arrFromListWithoutInt(l, k);
-  return listFromArr(arr);
-}
-
 class Queue {
   constructor() {
     this.node = null;
